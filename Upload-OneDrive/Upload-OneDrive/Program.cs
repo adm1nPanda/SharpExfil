3c565f187674c6bdd2c8754730abb5b5e903432d
Dushyanth Chowdary <dnc295@nyu.edu>
2019-08-01 15:09:19 -0400

    rewrote with code token api

--- Upload-OneDrive/Upload-OneDrive/Program.cs
+++ Upload-OneDrive/Upload-OneDrive/Program.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security;
 using System.Text;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Microsoft.Identity.Client;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.IO;
 using Newtonsoft.Json.Linq;
 
 
@@ -16,135 +11,124 @@ namespace Upload_OneDrive
 {
     class Program
     {
-        private static string clientId = "28c06217-61b6-4838-90a4-793ef51313ac";
-        private static string tenantId = "8d5ac6ca-308b-420d-a91f-05f879c4f8df";
+        
+        
+        static string cid = "28c06217-61b6-4838-90a4-793ef51313ac";
+        static string sco = "user.read.all files.readwrite.all offline_access";
+        static string ruri = "http://localhost/auth";
+        static string cs = "0KAtXXanUbM/[utu-UjaNQmniNGT=906";
 
-       
-        private static async Task<AuthenticationResult> GetATokenForGraph()
+
+
+        static void Main()
         {
 
-            string authority = $"https://login.microsoftonline.com/{tenantId}/";
-            string[] scopes = new string[] { "User.Read", "User.Read.All", "Files.ReadWrite.All" };
-            IPublicClientApplication app = PublicClientApplicationBuilder
-                .Create(clientId)
-                .WithAuthority(authority)
-                .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/token")
-                .Build();
+            //authuntication using code token
+            Console.WriteLine($"Authenticate at URL - https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id={cid}&scope={sco}&response_type=code&redirect_uri={ruri} . Once completed copy the code token from the browser address bar and input into application");   
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create($"https://login.microsoftonline.com/common/oauth2/v2.0/token");
+            var code = Console.ReadLine();
+            string postData = $"client_id={cid}&redirect_uri={ruri}&client_secret={cs}&code={code}&grant_type=authorization_code";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Method = "POST";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+
+            Stream outStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(outStream);
+            string responseFromServer = reader.ReadToEnd();
+            dynamic resp = JObject.Parse(responseFromServer);
+            
+            response.Close();
 
 
-            AuthenticationResult result = null;
-            var accounts = await app.GetAccountsAsync();
 
-            // All AcquireToken* methods store the tokens in the cache, so check the cache first
-            try
+
+            // Requesting URL for large file upload
+            string upload = "https://graph.microsoft.com/v1.0/drive/root:/testfoldera:/createUploadSession";
+            
+            HttpWebRequest rq1 = (HttpWebRequest)HttpWebRequest.Create(upload);
+            rq1.Method = "POST";
+            rq1.Host = "graph.microsoft.com";
+            rq1.Headers.Add("Authorization", "Bearer " + resp.access_token);
+            rq1.Accept = "application/json, text/plain, */*";
+            rq1.ContentType = "application/json";
+            
+            String a = "{" +
+                            "\"item\": {" +
+                                        "\"@odata.type\": \"microsoft.graph.driveItemUploadableProperties\"," +
+                                        "\"@microsoft.graph.conflictBehavior\": \"rename\"," +
+                                        "\"name\": \"largefile.dat\"" +
+                            "}" +
+                        "}";
+
+            byte[] data = Encoding.ASCII.GetBytes(a);
+            rq1.ContentLength = data.Length;
+            using (var requestStream = rq1.GetRequestStream())
             {
-                result = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault())
-                    .ExecuteAsync();
-                return result;
+                requestStream.Write(data, 0, data.Length);
             }
-            catch (MsalUiRequiredException ex)
-            {
-                System.Diagnostics.Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
-            }
 
-            try
-            {
-                result = await app.AcquireTokenWithDeviceCode(scopes,
-                      deviceCodeCallback =>
-                      {
+            WebResponse rs1 = rq1.GetResponse();
+            outStream = rs1.GetResponseStream();
+            reader = new StreamReader(outStream);
+            responseFromServer = reader.ReadToEnd();
+            dynamic resp1 = JObject.Parse(responseFromServer);
+            Console.WriteLine("Received UploadURL: {0}",resp1.uploadUrl);
+            rs1.Close();
 
-                          Console.WriteLine(deviceCodeCallback.Message);
-                          return Task.FromResult(0);
-                      }).ExecuteAsync();
+            /*
+            // Read bytes of file to upload
+            String file_data = "Apples Mangoes Oranges Watermelon";
+            byte[] fdata = Encoding.ASCII.GetBytes(file_data);
 
-                Console.WriteLine("Received token from {0}", result.Account.Username);
-                return result;
-            }
-            catch (MsalServiceException ex)
-            {
-                Console.WriteLine("Error : {0}", ex.Message);
-            }
-            catch (OperationCanceledException)
-            {
-                Console.WriteLine("Operation Cancelled");
-            }
-            catch (MsalClientException)
-            {
-                Console.WriteLine("Verification Code Expired");
-            }
+            var flen = fdata.Length;
 
-            return null;
-        }
-
-        static void Main(string[] args)
-        {
-            try
+            //Uploading File few bytes at a time to OneDrive
+            var psize = 0;
+            if (flen < 1024)
             {
-                RunAsync().GetAwaiter().GetResult();
+                psize = 1024;
             }
-            catch (Exception ex)
+            else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(ex);
+                psize = flen;
+            }
+            var temp_upload_url = resp1.uploadUrl;
+            var brstart = 0;
+            var brend = psize;
+            for (var i=0; i<flen/psize; i++) {
+                HttpWebRequest rq2 = (HttpWebRequest)HttpWebRequest.Create(temp_upload_url);
+                rq2.Method = "PUT";
+                rq2.Host = "graph.microsoft.com";
+                rq2.Headers.Add("Authorization", "Bearer " + resp.access_token);
+                rq2.ContentLength = psize;
+                brstart = (brend*i);
+                brend = (brend*(i+1))-1;
+                Console.WriteLine($"Sending bytes {brstart}-{brend}/{flen}");
+                rq2.Headers.Add("ContentRange", $"bytes {brstart}-{brend}/{flen}");
                 
-                Console.ResetColor();
-            }
+                using (var requestStream = rq2.GetRequestStream())
+                {
+                    requestStream.Write(fdata, brstart, psize);
+                }
 
+                WebResponse rs2 = rq1.GetResponse();
+                outStream = rs2.GetResponseStream();
+                reader = new StreamReader(outStream);
+                responseFromServer = reader.ReadToEnd();
+                dynamic resp2 = JObject.Parse(responseFromServer);
+                Console.WriteLine("Received UploadURL: {0}", resp2.nextExpectedRanges);
+                rs2.Close();
+                
+
+            }*/
         }
 
-
-        private static async Task RunAsync()
-        {
-            AuthenticationResult aa = await GetATokenForGraph();
-            
-            
-
-            string[] uris = { "https://graph.microsoft.com/v1.0/me", "https://graph.microsoft.com/v1.0/me/events", "https://graph.microsoft.com/v1.0/users", "https://graph.microsoft.com/v1.0/drive/root", "https://graph.microsoft.com/v1.0/me/drive/root/children" };
-            foreach (string uri in uris)
-            {
-                var HttpClient = new HttpClient();
-
-                var defaultRequestHeaders = HttpClient.DefaultRequestHeaders;
-                if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
-                {
-                    HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                }
-                defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", aa.AccessToken);
-
-                Console.WriteLine("Response from query - {0}",uri);
-                HttpResponseMessage response = await HttpClient.GetAsync(uri);
-                if (response.IsSuccessStatusCode)
-                {
-                    string json = await response.Content.ReadAsStringAsync();
-                    JObject result = JsonConvert.DeserializeObject(json) as JObject;
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.WriteLine(result.ToString());
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    string content = await response.Content.ReadAsStringAsync();
-
-                    // It's ok to not have a manager
-                    if (!content.Contains("Resource 'manager' does not exist"))
-                    {
-                        Console.WriteLine($"Failed to call the Web Api: {response.StatusCode}");
-                        Console.WriteLine($"Content: {content}");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.WriteLine("No manager");
-                    }
-                }
-                Console.ResetColor();
-
-
-            }
-
-
-
-        }
     }
 }
 
