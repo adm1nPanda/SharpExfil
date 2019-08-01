7a87c55379f6fdc57556e3f49e7a1f17311b7039
Dushyanth Chowdary <dnc295@nyu.edu>
2019-08-01 11:36:35 -0400

    backup

--- Upload-OneDrive/Upload-OneDrive/Program.cs
+++ Upload-OneDrive/Upload-OneDrive/Program.cs
@@ -4,8 +4,13 @@ using System.Linq;
 using System.Security;
 using System.Text;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Microsoft.Identity.Client;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 
 namespace Upload_OneDrive
 {
@@ -14,17 +19,16 @@ namespace Upload_OneDrive
         private static string clientId = "28c06217-61b6-4838-90a4-793ef51313ac";
         private static string tenantId = "8d5ac6ca-308b-420d-a91f-05f879c4f8df";
 
-
-
+       
         private static async Task<AuthenticationResult> GetATokenForGraph()
         {
 
             string authority = $"https://login.microsoftonline.com/{tenantId}/";
-            string[] scopes = new string[] { "User.Read", "User.ReadWrite", "Files.Read", "Files.Read.All", "Files.Read.Selected", "Files.ReadWrite", "Files.ReadWrite.All", "Files.ReadWrite.Selected", "Files.ReadWrite.AppFolder" };
+            string[] scopes = new string[] { "User.Read", "User.Read.All", "Files.ReadWrite.All" };
             IPublicClientApplication app = PublicClientApplicationBuilder
                 .Create(clientId)
                 .WithAuthority(authority)
-                .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+                .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/token")
                 .Build();
 
 
@@ -81,7 +85,7 @@ namespace Upload_OneDrive
             catch (Exception ex)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex);
                 
                 Console.ResetColor();
             }
@@ -92,33 +96,54 @@ namespace Upload_OneDrive
         private static async Task RunAsync()
         {
             AuthenticationResult aa = await GetATokenForGraph();
+            
+            
 
-
-            using (var client = new System.Net.WebClient())
+            string[] uris = { "https://graph.microsoft.com/v1.0/me", "https://graph.microsoft.com/v1.0/me/events", "https://graph.microsoft.com/v1.0/users", "https://graph.microsoft.com/v1.0/drive/root", "https://graph.microsoft.com/v1.0/me/drive/root/children" };
+            foreach (string uri in uris)
             {
-                var data = Encoding.ASCII.GetBytes("AAAAAAAAAAAA");
+                var HttpClient = new HttpClient();
 
-                client.Headers["Host"] = "graph.microsoft.com";
-                //client.Headers["Content-Type"] = "text/plain";
-                client.Headers["Authorization"] = aa.CreateAuthorizationHeader();
-                //client.Headers["User-Agent"] = "Mozilla / 5.0(Macintosh; Intel Mac OS X 10_14_6) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 75.0.3770.142 Safari / 537.36";
-                //client.Headers["Accept"] = "application/json, text/plain, */*";
-                
+                var defaultRequestHeaders = HttpClient.DefaultRequestHeaders;
+                if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
+                {
+                    HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                }
+                defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", aa.AccessToken);
 
+                Console.WriteLine("Response from query - {0}",uri);
+                HttpResponseMessage response = await HttpClient.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    JObject result = JsonConvert.DeserializeObject(json) as JObject;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine(result.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    string content = await response.Content.ReadAsStringAsync();
 
-                //Console.WriteLine(aa.CreateAuthorizationHeader());
-
-                Console.WriteLine(client.Headers.ToString());
-
-                string uri = "https://graph.microsoft.com/v1.0/me";
-                Console.WriteLine(client.DownloadString(uri));
-
-                //string uri = "https://graph.microsoft.com/v1.0/drive/root:/testfoldera/Filez.txt:/content";
-                //client.UploadData(uri, "PUT", data);
-
+                    // It's ok to not have a manager
+                    if (!content.Contains("Resource 'manager' does not exist"))
+                    {
+                        Console.WriteLine($"Failed to call the Web Api: {response.StatusCode}");
+                        Console.WriteLine($"Content: {content}");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("No manager");
+                    }
+                }
+                Console.ResetColor();
 
 
             }
+
+
+
         }
     }
 }
